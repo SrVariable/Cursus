10462a84d3faf2339bbd35bc6599b05abccee541
SrRecursive <rojohnibana@gmail.com>
2023-05-08 03:19:31 +0200

    ft_lstiter added with signature

--- libft/ft_lstiter.c
+++ libft/ft_lstiter.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstiter.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ribana-b < ribana-b@student.42malaga.co    +#+  +:+       +#+        */
+/*   By: ribana-b <ribana-b@42student.malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 22:32:45 by ribana-b          #+#    #+#             */
-/*   Updated: 2023/05/05 23:07:58 by ribana-b         ###   ########.fr       */
+/*   Updated: 2023/05/08 02:49:01 by ribana-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,3 +24,23 @@ void	ft_lstiter(t_list *lst, void (*f)(void *))
 		lst = (*lst).next;
 	}
 }
+
+/*
+     RRRRR                            
+   RRR   RRR                          
+  RRR     RRR                         
+  RR      RRRRRRRRRRRRRRRR            
+  RRR     RRRR          RRRRRR        
+   RRR    RRR               RRR       
+     RRRRRRRR               RRRR      
+          RRR            RRRRR        
+          RRR            RRR          
+          RRR          RRRR           
+         RRR       RRRRR              
+         RRRRRRRRRRRRRR               
+       RRRR          RRRR             
+      RRRR             RRR            
+     RRRR               RRR           
+    RRR                  RRR     RRRRR
+  RRRR                    RRRRRRRR    
+*/
