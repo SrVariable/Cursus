d42156a1b8fea1cdd997836505387eb759543c6a
SrRecursive <rojohnibana@gmail.com>
2023-05-08 03:19:41 +0200

    ft_lstlast added with signature

--- libft/ft_lstlast.c
+++ libft/ft_lstlast.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstlast.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ribana-b < ribana-b@student.42malaga.co    +#+  +:+       +#+        */
+/*   By: ribana-b <ribana-b@42student.malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 14:15:26 by ribana-b          #+#    #+#             */
-/*   Updated: 2023/05/05 22:36:19 by ribana-b         ###   ########.fr       */
+/*   Updated: 2023/05/08 02:49:04 by ribana-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,3 +27,23 @@ t_list	*ft_lstlast(t_list *lst)
 	}
 	return (lst);
 }
+
+/*
+     RRRRR                            
+   RRR   RRR                          
+  RRR     RRR                         
+  RR      RRRRRRRRRRRRRRRR            
+  RRR     RRRR          RRRRRR        
+   RRR    RRR               RRR       
+     RRRRRRRR               RRRR      
+          RRR            RRRRR        
+          RRR            RRR          
+          RRR          RRRR           
+         RRR       RRRRR              
+         RRRRRRRRRRRRRR               
+       RRRR          RRRR             
+      RRRR             RRR            
+     RRRR               RRR           
+    RRR                  RRR     RRRRR
+  RRRR                    RRRRRRRR    
+*/
