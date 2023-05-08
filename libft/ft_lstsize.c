748c57e72a859228350cff888ff44465e6b90f2f
SrRecursive <rojohnibana@gmail.com>
2023-05-08 03:20:14 +0200

    ft_lstsize added with signature

--- libft/ft_lstsize.c
+++ libft/ft_lstsize.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstsize.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ribana-b < ribana-b@student.42malaga.co    +#+  +:+       +#+        */
+/*   By: ribana-b <ribana-b@42student.malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 13:57:17 by ribana-b          #+#    #+#             */
-/*   Updated: 2023/05/05 22:39:06 by ribana-b         ###   ########.fr       */
+/*   Updated: 2023/05/08 02:49:13 by ribana-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,3 +28,23 @@ int	ft_lstsize(t_list *lst)
 	}
 	return (cont);
 }
+
+/*
+     RRRRR                            
+   RRR   RRR                          
+  RRR     RRR                         
+  RR      RRRRRRRRRRRRRRRR            
+  RRR     RRRR          RRRRRR        
+   RRR    RRR               RRR       
+     RRRRRRRR               RRRR      
+          RRR            RRRRR        
+          RRR            RRR          
+          RRR          RRRR           
+         RRR       RRRRR              
+         RRRRRRRRRRRRRR               
+       RRRR          RRRR             
+      RRRR             RRR            
+     RRRR               RRR           
+    RRR                  RRR     RRRRR
+  RRRR                    RRRRRRRR    
+*/
