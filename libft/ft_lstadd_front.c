6623c6ec331a92c9a0987da2b2578e4447036743
SrRecursive <rojohnibana@gmail.com>
2023-05-08 03:19:03 +0200

    ft_lstadd_front added with signature

--- libft/ft_lstadd_front.c
+++ libft/ft_lstadd_front.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstadd_front.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ribana-b < ribana-b@student.42malaga.co    +#+  +:+       +#+        */
+/*   By: ribana-b <ribana-b@42student.malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 12:58:26 by ribana-b          #+#    #+#             */
-/*   Updated: 2023/05/05 22:51:32 by ribana-b         ###   ########.fr       */
+/*   Updated: 2023/05/08 02:48:51 by ribana-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,3 +21,23 @@ void	ft_lstadd_front(t_list **lst, t_list *new)
 	(*new).next = *lst;
 	*lst = new;
 }
+
+/*
+     RRRRR                            
+   RRR   RRR                          
+  RRR     RRR                         
+  RR      RRRRRRRRRRRRRRRR            
+  RRR     RRRR          RRRRRR        
+   RRR    RRR               RRR       
+     RRRRRRRR               RRRR      
+          RRR            RRRRR        
+          RRR            RRR          
+          RRR          RRRR           
+         RRR       RRRRR              
+         RRRRRRRRRRRRRR               
+       RRRR          RRRR             
+      RRRR             RRR            
+     RRRR               RRR           
+    RRR                  RRR     RRRRR
+  RRRR                    RRRRRRRR    
+*/
