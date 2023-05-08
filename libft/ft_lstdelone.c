9692612310dae08c02d0b66cbaa90fae0aa3a4fc
SrRecursive <rojohnibana@gmail.com>
2023-05-08 03:19:22 +0200

    ft_lstdelone added with signature

--- libft/ft_lstdelone.c
+++ libft/ft_lstdelone.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstdelone.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ribana-b < ribana-b@student.42malaga.co    +#+  +:+       +#+        */
+/*   By: ribana-b <ribana-b@42student.malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 16:01:53 by ribana-b          #+#    #+#             */
-/*   Updated: 2023/05/05 22:49:26 by ribana-b         ###   ########.fr       */
+/*   Updated: 2023/05/08 02:48:58 by ribana-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,3 +21,23 @@ void	ft_lstdelone(t_list *lst, void (*del)(void *))
 	del((*lst).content);
 	free(lst);
 }
+
+/*
+     RRRRR                            
+   RRR   RRR                          
+  RRR     RRR                         
+  RR      RRRRRRRRRRRRRRRR            
+  RRR     RRRR          RRRRRR        
+   RRR    RRR               RRR       
+     RRRRRRRR               RRRR      
+          RRR            RRRRR        
+          RRR            RRR          
+          RRR          RRRR           
+         RRR       RRRRR              
+         RRRRRRRRRRRRRR               
+       RRRR          RRRR             
+      RRRR             RRR            
+     RRRR               RRR           
+    RRR                  RRR     RRRRR
+  RRRR                    RRRRRRRR    
+*/
