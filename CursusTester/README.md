3a0f974d148419fffab2e18e46167915271389da
SrRecursive <rojohnibana@gmail.com>
2023-06-16 19:13:12 +0200

    CursusTester added

---
+++ CursusTester/README.md
@@ -0,0 +1,3 @@
+# CursusTester
+
+This is my own Tester for 42 Cursus projects
---
+++ CursusTester/colors.h
@@ -0,0 +1,33 @@
+#ifndef COLORS_H
+# define COLORS_H
+
+// Text color
+# define T_BLACK  "\033[30m"
+# define T_RED "\033[31m"
+# define T_GREEN "\033[32m"
+# define T_YELLOW "\033[33m"
+# define T_BLUE "\033[34m"
+# define T_MAGENTA "\033[35m"
+# define T_CYAN "\033[36m"
+# define T_WHITE "\033[37m"
+
+// Text style
+# define BOLD "\033[1m"
+# define ITALIC "\033[3m"
+# define UNDERLINE "\033[4m"
+# define STRIKETHROUGH "\033[9m"
+
+// Background color
+# define B_BLACK "\033[40m"
+# define B_RED "\033[41m"
+# define B_GREEN "\033[42m"
+# define B_YELLOW "\033[43m"
+# define B_BLUE "\033[44m"
+# define B_MAGENTA "\033[45m"
+# define B_CYAN "\033[46m"
+# define B_WHITE "\033[47m"
+
+// Reset everything
+# define RESET "\033[0m"
+
+#endif
